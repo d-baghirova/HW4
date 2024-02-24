fd6d1fbaea332d2155490ba7c5cc7dffb0cc9a39
Diana Baghirova <diana.baghirova18@gmail.com>
2024-02-24 13:41:39 +0400

    content added

---
+++ ContentPage/src/components/NavBar.js
@@ -0,0 +1,35 @@
+import React from 'react'
+import { StyleSheet, View, Text } from 'react-native'
+
+const NavBar = () => {
+  return (
+    <View style={styles.container}>
+        <Text style={styles.link}>Back</Text>
+        <Text style={styles.heading}>Content</Text>
+        <Text style={styles.link}>Filter</Text>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+    container: {
+        height: 40,
+        width: "100%",
+        flexDirection: "row",
+        justifyContent: "space-between",
+        alignItems: "center",
+        marginBottom: 20,
+        marginTop: 30
+    },
+    link: {
+        color: "green",
+        fontSize: 16,
+        marginHorizontal: 10
+    }, 
+    heading: {
+        fontSize: 20,
+        fontWeight: "700"
+    }
+})
+
+export default NavBar
